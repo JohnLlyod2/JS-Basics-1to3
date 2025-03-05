3638a40daa2563e7330af864c581a1604953a798
JohnLlyod2 <zukimonte@gmail.com>
2025-03-05 21:25:20 +0800

    Update main.js

--- JS-BASICS 3/main.js
+++ JS-BASICS 3/main.js
@@ -1,19 +1,32 @@
-const elementTxt = document.getElementById("elements");
-const sumTxt = document.getElementById("sum");
+let numbers = [];
 
-let arr = [];
-let sum = 0;
-let high = 0;
-let low = 0;
-
-
-
-for(let i=0; i<arr.length; i++ ) {
-    sum += arr[i];
-    high += arr[i];
-    low += arr[i];
+function insert() {
+    const num1 = document.getElementById('num1').value;
+    if (num1 !== '') {
+        const number = parseFloat(num1);
+        if (!isNaN(number)) {
+            numbers.push(number);
+            updateDisplay();
+        } else {
+            alert('Please enter a valid number');
+        }
+    } else {
+        alert('Please enter a number');
+    }
 }
 
-elementTxt.innerHTML = arr.join("<br>");
-sumTxt.innerHTML = sum;
-console.log(arr);
+function deleteAll() {
+    numbers = [];
+    updateDisplay();
+}
+
+function updateDisplay() {
+    document.getElementById('elements').innerText = numbers.join(', ');
+    document.getElementById('sum').innerText = numbers.reduce((a, b) => a + b, 0);
+    document.getElementById('highest').innerText = Math.max(...numbers);
+    document.getElementById('lowest').innerText = Math.min(...numbers);
+
+    document.getElementById('sum').style.color = 'blue';
+    document.getElementById('highest').style.color = 'green';
+    document.getElementById('lowest').style.color = 'red';
+}
